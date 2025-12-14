e05d3e2daf2b7183f53ca5e8b141597d48aa0728
Vodka <e.pipeliners@gmail.com>
2025-12-14 09:53:52 +0700

    Perubahan sheet url

--- api/katalog.js
+++ api/katalog.js
@@ -1,3 +1,8 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+if (req.method === 'OPTIONS') return res.status(204).end();
+
 export default async function handler(req, res) {
   const sheetUrl = process.env.SHEET_URL;
 
